da49f6c3457bbf6391ed0fbbc2a4c54c46b0d527
Gonçalo Teixeira <gspteixeira@gmail.com>
2023-01-15 20:47:30 +0000

    remove smart constructors and add escape character

--- app/Parser.hs
+++ app/Parser.hs
@@ -4,32 +4,37 @@ import Text.ParserCombinators.Parsec
 import Text.ParserCombinators.Parsec.Expr
 import Lang
 
+reserved :: [Char]
+reserved = "().*+?!&|\\"
 
 parser :: Parser Re
 parser = buildExpressionParser table lit
 
 table :: [[Operator Char st Re]]
 table = 
-    [ [ Postfix (rStr <$ char '*')
-      , Postfix (rMay <$ char '?')
-      , Postfix (rPls <$ char '+')
-      , Prefix  (rNot <$ char '!')
+    [ [ Postfix (Str <$ char '*')
+      , Postfix (may <$ char '?')
+      , Postfix (pls <$ char '+')
+      , Prefix  (Not <$ char '!')
       ]
-    , [ Infix (return rSeq     ) AssocLeft
-      ]
-    , [ Infix (rAlt <$ char '|') AssocLeft
-      , Infix (rAnd <$ char '&') AssocLeft
+    , [ Infix   (return Seq     ) AssocLeft
+      ]  
+    , [ Infix   (Alt <$ char '|') AssocLeft
+      , Infix   (And <$ char '&') AssocLeft
       ]
     ]
 
 lit :: Parser Re
-lit = dot <|> sym <|> paren
+lit = sym <|> dot <|> paren
 
 paren :: Parser Re
 paren = between (char '(') (char ')') parser
 
 sym :: Parser Re
-sym = Sym <$> noneOf "().*!&|"
+sym = Sym <$> (symbols <|> escaped)
+  where
+    escaped = char '\\' *> oneOf reserved
+    symbols = noneOf reserved
 
 dot :: Parser Re
 dot = Dot <$ char '.'
