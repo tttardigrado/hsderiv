37e11b7cf67552596c31f259016492fd2e879fdf
Gonçalo Teixeira <gforce4760@gmail.com>
2023-01-22 00:40:33 +0000

    Edit main file

--- app/Main.hs
+++ app/Main.hs
@@ -2,9 +2,10 @@
 module Main where
 
 import System.Console.Haskeline
-import Parser
-import Lang
-import Derivative
+    ( defaultSettings, getInputLine, outputStrLn, runInputT, InputT )
+import Parser ( parseRegex )
+import Lang ( Re(Nil) )
+import Derivative ( matches )
 
 pattern Quit :: Maybe String
 pattern Quit <- Just ":q"
@@ -13,17 +14,17 @@ pattern Print :: Maybe String
 pattern Print <- Just ":p"
 
 pattern Set :: [Char] -> Maybe [Char]
-pattern Set re <- Just (':':'r':' ':re)
+pattern Set re <- Just (':':'s':' ':re)
 
 loop :: Re -> InputT IO ()
 loop regex = do
-    line <- getInputLine "user> "
+    line <- getInputLine "hsderiv> "
     case line of
         Nothing  -> return ()
         Quit     -> return ()
         Print    -> outputStrLn (show regex) >> loop regex
         Set re   -> case parseRegex re of
-            Right r -> loop r
+            Right r -> outputStrLn (show r) >> loop r
             Left  m -> outputStrLn (show m) >> loop regex
         Just str -> do
             outputStrLn $ show $ matches str regex
